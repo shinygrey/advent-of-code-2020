af9467cbd662c82721f2888a4bc1392d2d416843
Shiny Grey Ltd <mac@shinygrey.com>
2020-12-26 21:36:01 +0000

    es2017 features

---
+++ scratchpad.js
@@ -0,0 +1,9 @@
+var one = {a:1,b:2,c:3,d:"red",e:"blue",f:"green",g:"yellow",h:false,i:true,j:null};
+
+var two = Object.entries(one);
+console.log("Entries",two);
+
+var three = Object.values(one);
+console.log("Values",three);
+
+
