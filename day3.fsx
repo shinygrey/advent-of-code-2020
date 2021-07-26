74a4a186a1269e7cfc8862601bc1690500324529
Shiny Grey Ltd <mac@shinygrey.com>
2021-07-26 23:13:01 +0100

    part 1 correct and part 2 in progress

--- day3.fsx
+++ day3.fsx
@@ -3,7 +3,38 @@ open System
 let day3InputToLineSeq file =
     IO.File.ReadAllText file
     |> fun lines -> lines.Split Environment.NewLine
-    |> Array.toSeq
+
+let tobogganTrajectory (slope:String[]) right down =
+    let lineWidth = slope.[0] |> String.length
+    let rec getRightIndex rightIndexCheck =
+        if rightIndexCheck < lineWidth
+            then rightIndexCheck
+            else getRightIndex (rightIndexCheck - lineWidth)
+    let rec traverse rightAcc currentLine count =
+        let rightIndex = getRightIndex rightAcc
+        if Array.length slope <= currentLine + 1 // || Seq.length slope.[currentLine + 1] < 1
+            then count
+            else slope.[currentLine] |> (fun line -> Seq.item rightIndex line)
+                |> (fun x ->
+                    traverse (rightIndex + right) (currentLine + down) (if x = '#' then count + 1 else count))
+    traverse right down 0;;
+
+day3InputToLineSeq @"./day3_input.txt"
+    |> (fun lines -> tobogganTrajectory lines 3 1)
+    |> (fun answer -> printfn "answer: %A" answer);;
+
+// let slopes = [[|1;1|];[|3;1|];[|5;1|];[|7;1|];[|1;2|]]
+
+day3InputToLineSeq @"./day3_input.txt"
+    |> (fun lines -> //(slopes:list<int[]>)
+        let slopes = [[|1;1|];[|3;1|];[|5;1|];[|7;1|];[|1;2|]]
+        let rec multiplier acc =
+            match slopes with
+            | [hd] -> acc * tobogganTrajectory lines hd.[0] hd.[1]
+            | hd::_ -> multiplier acc * tobogganTrajectory lines hd.[0] hd.[1]
+            | _ -> raise (ArgumentException("other error 1"))
+        multiplier 1)
+
 
 (*
 let day3InputToList file =
@@ -16,25 +47,6 @@ let day3InputToLineArray file =
     IO.File.ReadAllText file
     |> fun lines -> lines.Split Environment.NewLine
 *)
-
-let tobogganTrajectory slope right down =
-    let lineWidth = Seq.item 0 slope |> Seq.length
-    let rec getRightIndex rightIndexCheck =
-        if rightIndexCheck < lineWidth
-            then rightIndexCheck
-            else getRightIndex (rightIndexCheck - lineWidth)
-    let rec traverse rightAcc currentLine count =
-        let rightIndex = getRightIndex rightAcc
-        if Seq.length slope <= currentLine
-            then count
-            else Seq.item currentLine slope |> (fun line -> Seq.item rightIndex line)
-                |> (fun x ->
-                    traverse (rightIndex + rightAcc) (currentLine + down) (if x = '#' then count + 1 else count))
-    traverse right 0 0;;
-
-day3InputToLineSeq @"./day3_input.txt"
-    |> (fun lines -> tobogganTrajectory lines 3 1)
-    |> (fun answer -> printfn "answer: %A" answer);;
 
 (*
 day3InputToList @"./day3_input.txt"
